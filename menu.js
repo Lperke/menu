2d381cdc287674b34d5787f870ee34fca51ef1d2
Lperke <juljagorb@gmail.com>
2016-11-01 10:26:59 +0200

    menu.js
    Add class to body to make mob menu active with css.

---
+++ menu.js
@@ -0,0 +1,33 @@
+(function($) {
+    $(document).ready(function () {
+    
+        $(document).on('click', '.nav-toogle', function (e) {
+            if ($(this).hasClass('state-active')) {
+                $(this).removeClass('state-active');
+                $('body').removeClass('state-active-nav');
+            } else {
+                $(this).addClass('state-active');
+                $('body').addClass('state-active-nav');
+            }
+            return false;
+        });
+
+        $(document).on('click', function (e) {  //close on click bg
+            if ($(window).width() < 786) {
+                if (!$(e.target).hasClass('nav-section') && !$(e.target).parents().hasClass('nav-section')) {
+                    $('.nav-toogle').removeClass('state-active');
+                    $('body').removeClass('state-active-nav');
+                }
+            }
+        });
+
+        $(window).resize(function () {  //hide on resize
+            if ($(window).width() > 785) {
+                $('body').removeClass('state-active-nav');
+                $('.nav-toggle').removeClass('state-active');
+            }
+
+        });
+     
+    });
+})(jQuery);
