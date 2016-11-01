22b7ade1c863733273a4dfd4fb1ebbbf50b65cfb
Lperke <juljagorb@gmail.com>
2016-11-01 12:49:21 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -27,6 +27,94 @@
                 $('.nav-toggle').removeClass('state-active');
             }
 
+			posDropSubmenu();
+        });
+		
+		
+		 $(document).on('click', '.nav ul a', function () {
+            if ($(this).parent().hasClass('has-children')) {
+                return false;
+            }
+        });
+		
+		//styled fixed menu
+        $(window).on('scroll', function () {
+            if ( $(window).scrollTop() == 0) {
+                $('.nav').removeClass('scrolled');
+            } else {
+                $('.nav').addClass('scrolled');
+            }
+        });
+
+
+		//if has children 
+		
+        var hasChildren = $('.has-children');
+
+        hasChildren.each(function() {
+            var children = $(this).find('.sub-menu');
+
+
+            children.on('mouseenter', function () {
+                    $(this).closest('.has-children').addClass('active');
+                })
+                .on('mouseleave', function () {
+                    $(this).closest('.has-children').removeClass('active');
+                });
+        });
+		
+		//fix dropdown width
+		posDropSubmenu();
+
+        function posDropSubmenu () {
+            $('.nav').find('.sub-sub-menu').each(function () {
+                var parOffset = $(this).closest('.sub-menu').closest('.has-children').offset().left;
+                var offset = parOffset + $(this).closest('.sub-menu').width() + $(this).width();
+                if (offset >= $(window).width()) {
+                    $(this).css({
+                        left: 'auto',
+                        right: '100%'
+                    });
+                }
+            });
+        }
+		
+		
+		//activate link if anchor == elem
+		var menuHeight = $('.nav li a').outerHeight();
+		var scrollTimeout;
+		var scrollLinks = $('.nav li a');
+		
+		$(window).scroll(function() {
+			clearTimeout(scrollTimeout);
+			scrollTimeout = setTimeout(function() {
+				scrollLinks.each(function(i) {
+					var obj = $($(this).attr('href'));
+
+					if (obj.size() > 0 && obj.offset().top <= $(window).scrollTop() + menuHeight) {
+						//when id and href - make active
+						scrollLinks.closest('li').removeClass('active');
+						$(this).closest('li').addClass('active');
+					}
+				});
+			}, 200);//speed of scroll
+		});
+		
+		//smooth scroll
+		 $(document).on('click', scrollLinks, function () {
+			var obj = $('');;
+			
+            if ($(this).attr('href').slice(0,1) == '#') {
+                obj = $($(this).attr('href'));
+            }
+
+            if (obj.size() > 0) {
+                $('html, body').animate({
+                    scrollTop: obj.offset().top - menuHeight
+                });
+            }
+
+            return false;
         });
      
     });
